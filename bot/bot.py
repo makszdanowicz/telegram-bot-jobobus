35e2240db18d8bd5545a9db3fc7fb3f7a2eb343b
Kyrylo Semenchenko <kyrylo.semenhcenko@gmail.com>
2025-01-05 16:39:58 +0100

    Fixed: typo in folder name, changed config -> .env file for better api token handling

--- bot/bot.py
+++ bot/bot.py
@@ -2,23 +2,30 @@ import json
 import logging
 from aiogram import Bot, Dispatcher
 
+import os
+from dotenv import load_dotenv
+
 from bot.handlers.start.start_handlers import start_router
 from bot.handlers.employee.employee_handlers import employee_router
 
 
-# function that load and read api token from config json file
-def load_api_token(config_path=r'bot/config/bot_config.json'):
-    try:
-        with open(config_path, 'r') as f:
-            config = json.load(f)
-            print(config.get('API_TOKEN'))
-        return config.get('API_TOKEN')  # Returning only token
-    except Exception as e:
-        logging.error(f"Failed to load config file {config_path}: {e}")
-        return None
+# # function that load and read api token from config json file
+# def load_api_token(config_path=r'bot/config/bot_config.json'):
+#     try:
+#         with open(config_path, 'r') as f:
+#             config = json.load(f)
+#             print(config.get('API_TOKEN'))
+#         return config.get('API_TOKEN')  # Returning only token
+#     except Exception as e:
+#         logging.error(f"Failed to load config file {config_path}: {e}")
+#         return None
 
 
-API_TOKEN = load_api_token()
+# API_TOKEN = load_api_token()
+
+load_dotenv()
+API_TOKEN = os.getenv("API_TOKEN")
+print(API_TOKEN)
 
 # Bot initialization
 bot = Bot(token=API_TOKEN)
