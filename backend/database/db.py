42bf205e5c263b41f155979979f3e908ab5ef3b0
makszdanowicz <273564@student.pwr.edu.pl>
2025-01-12 11:58:03 +0100

    initialize connection to db and change db-config file from .json to .env

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,18 +1,57 @@
+from typing import Optional
+
 import aiomysql
-import json
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env config file using specified path
+load_dotenv(dotenv_path="backend/database/db_config.env")
+
+# Read database configuration data from environment variables
+db_config_data = {
+    "host": os.getenv("DB_HOST"),
+    "port": int(os.getenv("DB_PORT")),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME")
+}
+
+# Declaration of pool of connections to the db
+pool: Optional[aiomysql.Pool] = None
 
 
-async def create_connection():
-    # Reading database configuration data from a JSON file
-    with open('backend/bot_config.json', 'r') as config_file:
-        config_data = json.load(config_file)
+# Function to create and initialize the connection pool
+async def create_pool():
+    global pool
 
-    # Creating database connection
-    connection = await aiomysql.connect(
-        host=config_data["host"],
-        user=config_data["user"],
-        password=config_data["password"],
-        db=config_data["database"]
+    # Create a connection pool with specified parameters from config
+    pool = await aiomysql.create_pool(
+        host=db_config_data["host"],
+        user=db_config_data["user"],
+        password=db_config_data["password"],
+        db=db_config_data["database"],
+        minsize=1,
+        maxsize=10,
+        autocommit=True
     )
 
-    return connection
+
+# Function to get a connection from the pool
+async def get_connection():
+    global pool
+    if pool is None:
+        raise Exception("Pool is not initialized. Call create_pool() first.")
+    async with pool.acquire() as connection:
+        print("Connection is initialized!")
+        return connection
+
+
+# Function to close the connection pool
+async def close_connection():
+    global pool
+    if pool:
+        # Close the pool and wait for all connections to be properly closed
+        pool.close()
+        await pool.wait_closed()
+    else:
+        print("Pool was not initialized or already closed.")
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import asyncio
 import logging
 from bot.bot import dp, bot
+from backend.database import create_pool, close_connection, get_connection
 
 
 async def main():
-    # Start polling - it means start listening for incoming messages and updated
-    await dp.start_polling(bot)
+    await create_pool()  # Initialize the database connection pool
+    try:
+        # Get the connection to the db
+        await get_connection()
+
+        # Start polling - it means start listening for incoming messages and updated
+        await dp.start_polling(bot)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        await close_connection()
+    finally:
+        # Ensure connection closure if the bot stops normally
+        await close_connection()
 
 
 # Bot execution entry point
