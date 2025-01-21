7fb2245e51d3cf9d625e14c4d0b665396a05b000
makszdanowicz <273564@student.pwr.edu.pl>
2025-01-21 01:14:26 +0100

    implement job offers display and user interaction

--- backend/database/employer/__init__.py
+++ backend/database/employer/__init__.py
@@ -1,4 +1,5 @@
 from .employer_queries import insert_employer, change_employer_company_name, select_employer_by_id, delete_employer
 from .job_offers_queries import (insert_job_offer, select_job_offer_by_id, delete_job_offer, 
 select_all_job_offers, update_job_offer_description, select_all_specializations,
-select_job_offer_by_id, select_job_offer_by_user_id)
+select_job_offer_by_id, select_job_offer_by_user_id, select_user_id_by_offer_id, select_matching_job_offers)
+from .notification_queries import insert_notification
